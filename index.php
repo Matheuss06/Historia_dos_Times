b9358b6a83e46fd6a2800fb8be731fce24746e9a
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-02-06 22:13:41 -0300

    Projeto Historia Times

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Document</title>
+</head>
+<body class="fundo">
+        <?php include 'header.html'; ?>
+     <main class="container">
+
+
+<div class="d-flex justify-content-around distancia">
+                    
+                    <img src="images/escudos/logo_saopaulo.png" alt="" class="escudos">
+                
+                    
+                    <img src="images/escudos/logo_palmeiras.png" alt="" class="escudos">
+                
+                    
+                    <img src="images/escudos/logo_corinthians.png" alt="" class="escudos">
+                
+                    
+                    <img src="images/escudos/logo_santos.png" alt="" class="escudos">
+</div>
+
+<div class="d-flex justify-content-around distancia">             
+                    <img src="images/escudos/logo_flamengo.png" alt="" class="escudos">
+                      
+                             
+                    <img src="images/escudos/logo_fluminense.png" alt="" class="escudos">
+                
+                    
+                    <img src="images/escudos/logo_vasco.png" alt="" class="escudos">
+                
+                    
+                    <img src="images/escudos/logo_botafogo.png" class="escudos">
+            
+                           
+</div>
+     </main>
+
+     
+</body>
+</html>
